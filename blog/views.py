d0e6f625af2b4b749e827fbc27c26b62bb8d2350
Matt Hynes <matthynesdev@gmail.com>
2016-02-22 21:03:04 -0330

    Add similar posts feature

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail
 from django.core.paginator import PageNotAnInteger
 from django.core.paginator import Paginator, EmptyPage
 from django.core.urlresolvers import reverse
+from django.db.models import Count
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from taggit.models import Tag
@@ -58,8 +59,14 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',
+                                                                             '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
