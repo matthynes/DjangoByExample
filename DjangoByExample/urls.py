aa395619f0e80ff73ef3ed793168ae37691da8a6
Matt Hynes <matthynesdev@gmail.com>
2016-02-26 20:10:36 -0330

    Change index url to redirect to index of blog app.

--- DjangoByExample/urls.py
+++ DjangoByExample/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='blog'), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/',
         include('blog.urls', namespace='blog', app_name='blog')),
