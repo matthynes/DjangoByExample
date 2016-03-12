2ec119a9b9de8e0b911cb111d2aaf381892d8b99
Matt Hynes <matthynesdev@gmail.com>
2016-03-12 15:22:46 -0330

    Add sitemaps for blog app

--- DjangoByExample/urls.py
+++ DjangoByExample/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.views.generic import RedirectView
+
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='blog'), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/',
         include('blog.urls', namespace='blog', app_name='blog')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
